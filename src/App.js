abad2410c18670a8b1b4e1e79a46e8b25a865b40
Jenny Song <jenny.jieun.song@gmail.com>
2019-10-11 01:08:20 -0700

    add redux

--- src/App.js
+++ src/App.js
@@ -1,63 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
+import React from 'react';
+import Login from './Login.js';
+import Home from './Home.js';
 
-function App() {
-
-    const [time, setTime] = useState(0);
-    const [date, setDate] = useState(0);
-
-    const updateClock = () => {
-        const now = new Date();
-        const currentTime = now.toTimeString();
-        const currentDate = now.toDateString();
-        setTime(currentTime.substring(0, currentTime.indexOf(' ')));
-        setDate(currentDate.substring(0, currentDate.lastIndexOf(' ')));
-    }
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        window.open('https://ca.linkedin.com/in/jennyjsong', '_blank')
-    }
-
-    useEffect(() => {
-        updateClock();
-        setInterval(updateClock, 1000);
-    });
-
-    return (
-        <div class='bg'>
-            <div class='header'>
-                <div class='inline'>82%</div>
-                <div class='icon battery inline' />
-                <div class='icon wifi inline' />
-                <div class='inline'>{date}</div>
-                <div class='inline'>{time}</div>
-            </div>
-            <div class='middle'>
-                <div class='jennysong center' />
-                <div class='text-center name'>Jenny Song</div>
-                <form class='text-center' onSubmit={handleSubmit}>
-                    <input class='submitbox' type='image' src={require('./assets/icon-aarow2.png')} alt='Submit'/>
-                    <input class='textbox' type='password' placeholder='Enter Password'/>
-                    <input class='submitbox' type='image' src={require('./assets/icon-aarow1.png')} alt='Submit'/>
-                </form>
-            </div>
-            <div class='bottom'>
-                <div class='icon-blocks'>
-                    <img src={require('./assets/icon-power1.png')}/>
-                    <p>Sleep</p>
-                </div>
-                <div class='icon-blocks'>
-                    <img src={require('./assets/icon-power2.png')}/>
-                    <p>Restart</p>
-                </div>
-                <div class='icon-blocks'>
-                    <img src={require('./assets/icon-power3.png')}/>
-                    <p>Shut Down</p>
-                </div>
-            </div>
-        </div>
-    );
+export default function App() {
+    return <Login />
 }
-
-export default App;
