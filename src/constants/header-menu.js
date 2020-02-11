411ea4c4b30e4164b2f6f0011481efbe92c91dd9
Jenny Song <jenny.jieun.song@gmail.com>
2020-02-11 00:14:58 -0800

    test move to mac

--- src/constants/header-menu.js
+++ src/constants/header-menu.js
@@ -12,6 +12,18 @@ export default function(menu) {
                 { type: 'text', text: 'Shut Down...' },
                 { type: 'text', text: 'Log Out Jenny Song...' }
             ]
+        case "Finder":
+            return [
+                { type: 'text', text: 'About This Mac' },
+                { type: 'text', text: 'System Preference...'},
+                { type: 'text', text: 'Location' },
+                { type: 'text', text: 'App Store...' },
+                { type: 'text', text: 'Recent Items' },
+                { type: 'text', text: 'Sleep' },
+                { type: 'text', text: 'Restart...' },
+                { type: 'text', text: 'Shut Down...' },
+                { type: 'text', text: 'Log Out Jenny Song...' }
+            ]
         default:
             return [{type: 'text', text: menu}]
     }
